2181d01996a424e9d8cd3f5113dd75718292e163
ArjixWasTaken <53124886+ArjixWasTaken@users.noreply.github.com>
2023-01-19 14:52:37 +0000

    improved the provider logging a bit

--- src/providers/mod.rs
+++ src/providers/mod.rs
@@ -8,15 +8,25 @@ use reqwest_middleware::ClientWithMiddleware;
 macro_rules! provider_api {
     ($method:ident, $value:ident, $return:ty) => {
         pub async fn $method(client: &ClientWithMiddleware, provider_name: &str, $value: &str) -> $return {
+            let call_expr_repr = format!("{}::{}({})", provider_name, stringify!($method), stringify!(client, $value));
+            
+            crate::terminal::info(format!("Attempting to execute '{}'", call_expr_repr));
+
             let Some(ref result) = (match provider_name {
                 "animeonsen" => Some(animeonsen::$method((client, &$value)).await?),
                 _ => None,
             }) else {
-                crate::terminal::error(format!("Provider '{}' not found", provider_name));
+                crate::terminal::error(
+                    format!(
+                        "Failed to execute '{}', cause: Provider '{}' not found!",
+                        call_expr_repr,
+                        provider_name
+                    )
+                );
                 return None;
             };
 
-            crate::terminal::success(format!("Executed '{}::{}()'", provider_name, stringify!($method)));
+            crate::terminal::success(format!("Executed '{}'", call_expr_repr));
             return Some(result.clone());
         }
     };
