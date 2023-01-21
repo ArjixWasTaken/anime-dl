a295c2996a76b898b4d057a95e05cbd1602ef0c3
ArjixWasTaken <53124886+ArjixWasTaken@users.noreply.github.com>
2023-01-21 19:21:11 +0200

    Refactored the cmds (dl, watch)
    Made a util for selecting a single search result from a vector of search results.

--- src/cmds/dl.rs
+++ src/cmds/dl.rs
@@ -18,51 +18,7 @@ pub async fn command(client: &ClientWithMiddleware, args: &ArgMatches<'_>) -> Re
 
     let search_results = providers::search(client, provider, query).await?;
 
-    if search_results.is_empty() {
-        crate::terminal::error(
-            "No anime was found using that query, try again with another provider or keyword!",
-        );
-        bail!("NoSearchResults");
-    }
-
-    let mut chosen: &SearchResult;
-
-    if choice < 1 {
-        println!("{}", search_results_to_table(&search_results).to_string());
-        let mut input = "".to_string();
-        let mut first_loop = true;
-
-        loop {
-            if input.trim().len() >= 1 && input.trim().parse::<usize>().is_ok() {
-                let idx = input.trim().parse::<usize>().unwrap();
-                if idx > 0 && idx <= search_results.len() {
-                    break;
-                }
-            }
-
-            if !first_loop {
-                println!(
-                    "Please input a number within the range of 1-{}",
-                    search_results.len()
-                );
-            }
-
-            first_loop = false;
-            input = casual::prompt("Select an anime [1]: ")
-                .default("1".to_string())
-                .get();
-        }
-
-        chosen = search_results
-            .get(input.trim().parse::<usize>().unwrap() - 1)
-            .unwrap();
-    } else {
-        if choice > search_results.len() as i32 {
-            crate::terminal::error(format!("--choice/-c with the value of {}, is more than the number of the available search results ({})", choice, search_results.len()));
-            bail!("InvalidChoice");
-        }
-        chosen = search_results.get((choice - 1) as usize).unwrap();
-    }
+    let chosen = crate::utils::user_select_result(search_results, choice)?;
 
     let episodes = providers::get_episodes(client, provider, chosen.url.as_str());
     let episodes = episodes.await?;
--- src/cmds/watch.rs
+++ src/cmds/watch.rs
@@ -17,41 +17,7 @@ pub async fn command(client: &ClientWithMiddleware, args: &ArgMatches<'_>) -> Re
     let ep_range = args.value_of("episode").unwrap_or("1:");
 
     let search_results = providers::search(client, provider, query).await?;
-
-    let mut chosen: &SearchResult;
-
-    if choice < 1 {
-        println!("{}", search_results_to_table(&search_results).to_string());
-        let mut input = "".to_string();
-
-        loop {
-            if input.trim().len() >= 1 && input.trim().parse::<usize>().is_ok() {
-                let idx = input.trim().parse::<usize>().unwrap();
-                if idx > 0 && idx <= search_results.len() {
-                    break;
-                }
-
-                println!(
-                    "Please input a number within the range of 1-{}",
-                    search_results.len()
-                );
-            }
-
-            input = casual::prompt("Select an anime [1]: ")
-                .default("1".to_string())
-                .get();
-        }
-
-        chosen = search_results
-            .get(input.trim().parse::<usize>().unwrap() - 1)
-            .unwrap();
-    } else {
-        if choice > search_results.len() as i32 {
-            crate::terminal::error(format!("--choice/-c with the value of {}, is more than the number of the available search results ({})", choice, search_results.len()).as_str());
-            bail!("InvalidChoice");
-        }
-        chosen = search_results.get((choice - 1) as usize).unwrap();
-    }
+    let chosen = crate::utils::user_select_result(search_results, choice)?;
 
     let episodes = providers::get_episodes(client, provider, chosen.url.as_str()).await?;
 
