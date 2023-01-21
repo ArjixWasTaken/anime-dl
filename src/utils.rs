3e7501de8bb8b0310b791564f250484f959bd2df
ArjixWasTaken <53124886+ArjixWasTaken@users.noreply.github.com>
2023-01-21 19:21:11 +0200

    Refactored the cmds (dl, watch)
    Made a util for selecting a single search result from a vector of search results.

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,4 @@
-use anyhow::Result;
+use anyhow::{bail, Result};
 use comfy_table::presets::ASCII_FULL;
 use comfy_table::Table;
 use reqwest::header;
@@ -22,6 +22,56 @@ pub fn search_results_to_table(search_results: &Vec<SearchResult>) -> Table {
         ]);
     }
     return table;
+}
+
+pub fn user_select_result(search_results: Vec<SearchResult>, choice: i32) -> Result<SearchResult> {
+    if search_results.is_empty() {
+        crate::terminal::error(
+            "No anime was found using that query, try again with another provider or keyword!",
+        );
+        bail!("NoSearchResults");
+    }
+
+    let mut chosen: &SearchResult;
+
+    if choice < 1 {
+        println!("{}", search_results_to_table(&search_results).to_string());
+        let mut input = "".to_string();
+        let mut first_loop = true;
+
+        loop {
+            if input.trim().len() >= 1 && input.trim().parse::<usize>().is_ok() {
+                let idx = input.trim().parse::<usize>().unwrap();
+                if idx > 0 && idx <= search_results.len() {
+                    break;
+                }
+            }
+
+            if !first_loop {
+                println!(
+                    "Please input a number within the range of 1-{}",
+                    search_results.len()
+                );
+            }
+
+            first_loop = false;
+            input = casual::prompt("Select an anime [1]: ")
+                .default("1".to_string())
+                .get();
+        }
+
+        chosen = search_results
+            .get(input.trim().parse::<usize>().unwrap() - 1)
+            .unwrap();
+    } else {
+        if choice > search_results.len() as i32 {
+            crate::terminal::error(format!("--choice/-c with the value of {}, is more than the number of the available search results ({})", choice, search_results.len()));
+            bail!("InvalidChoice");
+        }
+        chosen = search_results.get((choice - 1) as usize).unwrap();
+    }
+
+    Ok(chosen.clone())
 }
 
 pub fn parse_episode_range(episode_range: &str, latest_ep: i32) -> Vec<i32> {
