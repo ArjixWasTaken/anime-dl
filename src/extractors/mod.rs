a013967bbdc2f8c0c11bff4f6cecda840fcc6d3a
ArjixWasTaken <53124886+ArjixWasTaken@users.noreply.github.com>
2023-01-21 20:45:00 +0200

    Implemented the extractors module
    Although nothing is extracted yet :^)

--- src/extractors/mod.rs
+++ src/extractors/mod.rs
@@ -1,7 +1,12 @@
 use crate::types::StreamLink;
+use async_recursion::async_recursion;
 use reqwest::Client;
 use reqwest_middleware::ClientWithMiddleware;
+use url::Url;
 
+mod goload;
+
+#[async_recursion]
 pub async fn unpack_streams(
     client: &ClientWithMiddleware,
     streams: Vec<StreamLink>,
@@ -14,8 +19,26 @@ pub async fn unpack_streams(
             continue;
         }
 
-        // TODO: Detect and call the appropriate extractor.
+        let Ok(url) = Url::parse(&stream.url) else {
+            continue;
+        };
+
+        let Some(hostname) = url.host_str() else {
+            continue;
+        };
+
+        let hostname = hostname.to_string();
+
+        unpacked_streams.extend(match hostname.as_str() {
+            "goload.pro" => goload::unpack(client, stream).await,
+            _ => vec![],
+        });
     }
 
-    unpacked_streams
+    if unpacked_streams.iter().all(|x| x.is_direct) {
+        unpacked_streams
+    } else {
+        // An extractor gave more embed links, so we need to unpack them again
+        unpack_streams(client, unpacked_streams).await
+    }
 }
